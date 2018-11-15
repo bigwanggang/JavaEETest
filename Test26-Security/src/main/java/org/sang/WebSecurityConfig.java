b4b9f02eb795d6a362202d7f4f61ead69dc9e898
bigwanggang <bigwanggang@163.com>
2018-11-15 12:08:18 +0800

    增加BCryptPasswordEncoder对密码的验证

--- Test26-Security/src/main/java/org/sang/WebSecurityConfig.java
+++ Test26-Security/src/main/java/org/sang/WebSecurityConfig.java
@@ -1,17 +1,23 @@
 package org.sang;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by sang on 2017/1/10.
  */
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Bean
     UserDetailsService customUserService() {
         return new CustomUserService();
@@ -19,7 +25,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService());
+        auth.userDetailsService(customUserService())
+                .passwordEncoder(bCryptPasswordEncoder)
+        ;
     }
 
     @Override
