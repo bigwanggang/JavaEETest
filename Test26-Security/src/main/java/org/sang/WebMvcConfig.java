e8b05030f6141bb618134d4d62ef0c2bd1b70573
bigwanggang <bigwanggang@163.com>
2018-11-15 12:07:36 +0800

    增加BCryptPasswordEncoder对密码的验证

--- Test26-Security/src/main/java/org/sang/WebMvcConfig.java
+++ Test26-Security/src/main/java/org/sang/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package org.sang;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,5 +14,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
     }
 }
