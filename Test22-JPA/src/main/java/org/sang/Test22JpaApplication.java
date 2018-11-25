11bdccc3082fe0df394a55065424c390a37e51a5
gustavoWill <bigwanggang@163.com>
2018-11-25 20:44:50 +0800

    Update Test22JpaApplication.java

--- Test22-JPA/src/main/java/org/sang/Test22JpaApplication.java
+++ Test22-JPA/src/main/java/org/sang/Test22JpaApplication.java
@@ -2,8 +2,10 @@ package org.sang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class Test22JpaApplication {
 
 	public static void main(String[] args) {
